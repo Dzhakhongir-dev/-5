9d344b08bed8e821a5e6cba17cba38339e6ff66d
Dzhakhongir <84761335+Dzhakhongir-dev@users.noreply.github.com>
2022-05-22 11:12:31 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,49 +1,48 @@
-﻿using System;
+using System;
 
 namespace Домашка5
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Please enter words to Split");
-      string input = Console.ReadLine();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter words to Split");
+            string input = Console.ReadLine();
 
-      string inputPhrase = "";
+            string inputPhrase = "";
 
-      ReverseWords(inputPhrase);
+            ReverseWords(inputPhrase);
+        }
+        public static string[] StringSpliter(string input, string inputPhrase)
+        {
+            foreach (var item in input.Split())
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("\nPlease Enter String to Reverse Them");
 
-      StringSpliter(input, inputPhrase);
+            StringSpliter(input, inputPhrase);
+
+            ReverseWords(inputPhrase);
+
+            return input.Split(input);
+        }
+        public static string[] ReverseWords(string inputPhrase)
+        {
+
+            inputPhrase = Console.ReadLine();
+            char[] inputStringChars = inputPhrase.ToCharArray();
+            int inputStringLenght = inputStringChars.Length;
+
+            string stringReverse = "";
+
+            for (int i = inputStringLenght - 1; i >= 0; i--)
+            {
+                stringReverse += inputPhrase[i];
+            }
+            Console.WriteLine(stringReverse);
+            return stringReverse.Split(inputPhrase);
+        }
 
     }
-    public static string[] StringSpliter(string input, string inputPhrase)
-    {
-      foreach (var item in input.Split())
-      {
-        Console.WriteLine(item);
-      }
-      Console.WriteLine("\nPlease Enter String to Reverse Them");
-
-      ReverseWords(inputPhrase);
-
-      return input.Split(input);
-    }
-    public static string[] ReverseWords(string inputPhrase)
-    {
-
-      inputPhrase = Console.ReadLine();
-      char[] inputStringChars = inputPhrase.ToCharArray();
-      int inputStringLenght = inputStringChars.Length;
-
-      string stringReverse = "";
-
-      for (int i = inputStringLenght - 1; i >= 0; i--)
-      {
-        stringReverse += inputPhrase[i];
-      }
-      Console.WriteLine(stringReverse);
-      return stringReverse.Split(inputPhrase);
-    }
-
-  }
 }
